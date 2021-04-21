6ea68d459c559acb3b4cdc8ba72d08d20ca6caf1
Adi <adithya.sekar@ibm.com>
2021-04-21 05:38:59 -0400

    Validation and refactor

---
+++ CSVAnalyzer/Utility/AllowedExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSVAnalyzer.Utility
+{
+    public class AllowedExtensions : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensions(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IFormFile file = null;
+            string extension = null;
+            if (value != null)
+            {
+                file = value as IFormFile;
+                extension = Path.GetExtension(file.FileName);
+            }
+
+            if (file != null)
+            {
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult($"The file extension is not allowed!");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- CSVAnalyzer/ViewModels/FileUploadVM.cs
+++ CSVAnalyzer/ViewModels/FileUploadVM.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CSVAnalyzer.Utility;
 
 namespace CSVAnalyzer.ViewModels
 {
     public class FileUploadVM
     {
+
+        [Required]
+        [AllowedExtensions(new string[] { ".csv" })]       
         public IFormFile CsvFile { get; set; }
     }
 }
